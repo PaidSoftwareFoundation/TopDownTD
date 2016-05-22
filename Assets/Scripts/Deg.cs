d60b3440c86b4c31243dc8fa5f12d1261a35b4ba
EliVerbrugge <eli.verbrugge@gmail.com>
2016-05-22 08:04:50 -0600

    Balance

--- Assets/Scripts/Deg.cs
+++ Assets/Scripts/Deg.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Deg : MonoBehaviour {
 
     public int stat = 150;
+
+    void Update()
+    {
+        if (stat <= 0)
+            Destroy(gameObject);
+    }
 	
     void OnTriggerEnter(Collider other)
     {
